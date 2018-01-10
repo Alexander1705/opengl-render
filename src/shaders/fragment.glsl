dd703ced76d8cc357503acb6e9830a3b70d643f7
Alexander Movchan <alexandermovchan1705@gmail.com>
2018-01-10 17:50:04 +0200

    Use Blinn-Phong shading

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,28 +2,28 @@
 
 out vec3 color;
 
-in vec4 c_position;
-in vec4 c_normal;
+in vec3 c_position;
+in vec3 c_normal;
 
 uniform vec3 c_light;
 
 const float intensity = 0.5;
 const float Ka = 0.25;
-const float Kd = 1.0;
+const float Kd = 0.75;
 const float Ks = 0.5;
-const float shininess = 5.0;
+const float shininess = 25.0;
 
 void main()
 {
     vec3 ambient = vec3(Ka * intensity);
 
-    float k = clamp(dot(c_light, c_normal.xyz), 0.0, 1.0);
+    float k = max(0.0, dot(c_light, c_normal.xyz));
     vec3 diffuse = vec3(Kd * k * intensity);
 
     vec3 c_view = normalize(- c_position.xyz);
-    vec3 c_reflection = reflect(-c_light, c_normal.xyz);
+    vec3 c_halfway = normalize(c_view + c_light);
 
-    k = clamp(dot(c_reflection, c_view), 0.0, 1.0);
+    k = max(0.0, dot(c_normal, c_halfway));
     vec3 specular = vec3(Ks * pow(k, shininess));
 
     color = ambient + diffuse + specular;
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -6,13 +6,13 @@ layout(location = 1) in vec3 m_normal;
 uniform mat4 ModelToScreen;
 uniform mat4 ModelToCamera;
 
-out vec4 c_position;
-out vec4 c_normal;
+out vec3 c_position;
+out vec3 c_normal;
 
 void main()
 {
     gl_Position = ModelToScreen * vec4(m_position, 1);
 
-    c_position = ModelToCamera * vec4(m_position, 1.0);
-    c_normal   = ModelToCamera * vec4(m_normal, 0.0);
+    c_position = (ModelToCamera * vec4(m_position, 1.0)).xyz;
+    c_normal   = (ModelToCamera * vec4(m_normal, 0.0)).xyz;
 }
